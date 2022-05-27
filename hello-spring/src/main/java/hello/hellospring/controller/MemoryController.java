8d2361d332370acab37fe40d7f8b28f61e1fd1bd
Jehyun Jung <toojey7@gmail.com>
2022-05-28 00:12:07 +0900

    feat: Member Management
    Controller
    Service
    Repository
    Domain

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemoryController.java
@@ -0,0 +1,19 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemoryController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemoryController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+
+
+
+}
